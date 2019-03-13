aeda2b279c84f6a97133cd426ad03f2cbdf12ac5
Your NameGreg <you@example.comwatkinsgljr@gmail.com>
2019-03-13 15:04:25 -0500

    updated logic and updated innerHTML commands to update user status upon guessing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,15 @@
 
 
 // Establishing variables 
-var words = ["test", "greg", "wonderful", "head", "slurp"];
+var words = ["test", "greg", "wonderful", "foot", "slurp"];
 var word = words[Math.floor(Math.random() * words.length)];
+// words[1] = "love";
+console.log(words);
 
-//Event listener that captures user input
-let userGuess = document.getElementById("user-guess");
- console.log(userGuess.textContent);
- document.onkeyup = function(event) {
-     console.log(event);
-     userGuess.textContent = event.key.toLowerCase();
-     console.log(userGuess.textContent);
- }
-
+let lettersRemaining = word.length;
+let attemptsRemaining = (word.length * 2);
+console.log(lettersRemaining)
+console.log(attemptsRemaining)
 
 //The answer bank at initial assignment of random word
 let answerBank = [];
@@ -21,10 +18,54 @@ for (var i = 0; i < word.length; i++) {
 }
 console.log(answerBank);
 
-let lettersRemaining = word.length;
-let attemptsRemaining = (word.length * 2);
-console.log(lettersRemaining)
-console.log(attemptsRemaining)
+
+
+
+//Event listener that captures user input
+let userGuess = document.getElementById("user-guess");
+let answerBankDiv = document.getElementById("answer-bank");
+answerBankDiv.innerHTML = answerBank.join(' ');
+let attemptsRemainingDiv = document.getElementById("attempts-remaining-holder");
+attemptsRemainingDiv.innerHTML = attemptsRemaining;
+let lettersRemainingDiv = document.getElementById("letters-remaining-holder");
+lettersRemainingDiv.innerHTML = lettersRemaining;
+
+console.log(userGuess.textContent);
+document.onkeyup = function (event) {
+    console.log(event);
+    userGuess.textContent = event.key.toLowerCase();
+    console.log(userGuess.textContent);
+
+    //Check user status
+
+    // }  else if (userGuess.textContent === /^[a-zA-z]/) {
+    //     alert("You may only select letters. Try again!");
+        attemptsRemaining--;
+        attemptsRemainingDiv.innerHTML = attemptsRemaining;
+        for (var j = 0; j < word.length; j++) {
+            if (word[j] === userGuess.textContent) {
+                answerBank[j] = userGuess.textContent;
+                lettersRemaining--;
+                answerBankDiv.innerHTML = answerBank.join(' ');
+                lettersRemainingDiv.innerHTML = lettersRemaining;
+            } if (lettersRemaining && attemptsRemaining === 0) {
+                alert("Clutch shot! Kobe at the buzzer for the win!");
+                //press button to start new game or cancel to end game.    
+            } else if (attemptsRemaining >= word.length && lettersRemaining === 0) {
+             alert("Great win! It's a blowout victory for the Lakers!");
+                //Press button to try again or cancel to end game.
+            } else if (attemptsRemaining > 0 && lettersRemaining === 0) {
+                alert("Lakers win! Great team victory!");
+            } else if (attemptsRemaining === 0 && lettersRemaining > 1) {
+                alert("You lose. Don't quit, try again!")
+            }
+        }
+    } 
+
+
+
+
+
 
 // Gamplay continuation requirements
 // while (lettersRemaining > 0 && attemptsRemaining > 0)
@@ -34,11 +75,11 @@ console.log(attemptsRemaining)
 
 
 //Guesses replace _ in answer bank
-if (userGuess.textContent !== 1) {
-    alert("Please press only one characterat a time");
+if (userGuess.textContent > 1) {
+    alert("Please press only one character at a time");
 } else {
     for (var j = 0; j < word.length; j++) {
-        if (word[j] == userGuess.textContent) {
+        if (word[j] === userGuess.textContent) {
             answerBank[j] = userGuess.textContent;
             lettersRemaining--;
             console.log(answerBank);
@@ -54,9 +95,16 @@ if (userGuess.textContent !== 1) {
 // //update answer bank, remaining letters and attempts remaining
 //  }
 
- var fruits = ["Banana"];
- var energy = fruits.join(" ");
- console.log(energy);
+//  var fruits = ["Banana", "Pear", "Strawberry"];
+//  var energy = fruits.join(" ");
+//  console.log(energy);
+//  console.log(energy.split());
+//  console.log(energy.charAt(0));
+
+//  console.log(word.split('').join(','));
+
+//  console.log(word.split());
+ 
 
 
 
