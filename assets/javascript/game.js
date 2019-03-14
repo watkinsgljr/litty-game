549f0d4025c2deda5a84ff07d5f413e0a40b47e3
Your NameGreg <you@example.comwatkinsgljr@gmail.com>
2019-03-13 23:39:25 -0500

    completed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,13 @@
 
 
 // Establishing variables 
-var words = ["KOBE", "MAGIC", "STAPLES", "FISHER", "SHAQ", "PHIL"];
+var words = ["KOBE", "MAGIC", "STAPLES", "FISHER", "SHAQ", "PHIL", "BASKETBALL", "HARDWOOD", "LAKESHOW", "WORTHY", "FINALS", "CHAMPIONSHIPS", "PURPLE", "GOLD"];
 var word = words[Math.floor(Math.random() * words.length)];
-// words[1] = "love";
-console.log(words);
+var alphaArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 let lettersRemaining = word.length;
 let attemptsRemaining = (word.length * 2);
-console.log(lettersRemaining)
-console.log(attemptsRemaining)
+
 
 //The answer bank at initial assignment of random word
 let answerBank = [];
@@ -29,82 +27,77 @@ let attemptsRemainingDiv = document.getElementById("attempts-remaining-holder");
 attemptsRemainingDiv.innerHTML = attemptsRemaining;
 let lettersRemainingDiv = document.getElementById("letters-remaining-holder");
 lettersRemainingDiv.innerHTML = lettersRemaining;
+let letterChoseBank = document.getElementById("letters-chose");
+letterChoseBank.innerHTML = alphaArray.join(" ");
 
 console.log(userGuess.textContent);
 document.onkeyup = function (event) {
     console.log(event);
     userGuess.textContent = event.key.toUpperCase();
     console.log(userGuess.textContent);
+    const regex = new RegExp(/^[a-zA-z]/);
+    if (!regex.test(userGuess.textContent)) {
+        alert("You may only select letters. Try again!");
+        return;
+    }
 
-    //Check user status
+    let alphaArrayIndex = alphaArray.indexOf(userGuess.textContent);
+    if (alphaArrayIndex === -1) {
+        alert("You already selected the letter " + userGuess.textContent.toUpperCase() + ". Please select another letter.");
 
-    // }  else if (userGuess.textContent === /^[a-zA-z]/) {
-    //     alert("You may only select letters. Try again!");
-        attemptsRemaining--;
-        attemptsRemainingDiv.innerHTML = attemptsRemaining;
-        for (var j = 0; j < word.length; j++) {
-            if (word[j] === userGuess.textContent) {
-                answerBank[j] = userGuess.textContent;
-                lettersRemaining--;
-                answerBankDiv.innerHTML = answerBank.join(' ');
-                lettersRemainingDiv.innerHTML = lettersRemaining;
-            } if (lettersRemaining && attemptsRemaining === 0) {
-                alert("Clutch shot! Kobe at the buzzer for the win!");
-                //press button to start new game or cancel to end game.    
-            } else if (attemptsRemaining >= word.length && lettersRemaining === 0) {
-             alert("Great win! It's a blowout victory for the Lakers!");
-                //Press button to try again or cancel to end game.
-            } else if (attemptsRemaining > 0 && lettersRemaining === 0) {
-                alert("Lakers win! Great team victory!");
-            } else if (attemptsRemaining === 0 && lettersRemaining > 1) {
-                alert("You lose. Don't quit, try again!")
+    } else {
+        alphaArray[alphaArrayIndex] = "*";
+        letterChoseBank.innerHTML = alphaArray.join(" "); {
+
+            //Check user status
+
+            // }  else if (userGuess.textContent === /^[a-zA-z]/) {a
+            //     alert("You may only select letters. Try again!");
+            attemptsRemaining--;
+            attemptsRemainingDiv.innerHTML = attemptsRemaining;
+            for (var j = 0; j < word.length; j++) {
+                if (word[j] === userGuess.textContent) {
+                    answerBank[j] = userGuess.textContent;
+                    lettersRemaining--;
+                    answerBankDiv.innerHTML = answerBank.join(' ');
+                    lettersRemainingDiv.innerHTML = lettersRemaining;
+                }
+                if (lettersRemaining === 0 && attemptsRemaining === 0) {
+                    answerBankDiv.innerHTML = answerBank.join(' ');
+                    setTimeout(function() { if(!alert("Clutch shot! Kobe at the buzzer for the win!")) {
+                        window.location.reload();
+                    }; }, 500);
+                    break;
+
+                } else if (attemptsRemaining >= word.length && lettersRemaining === 0) {
+                    answerBankDiv.innerHTML = answerBank.join(' ');
+                    setTimeout(function() { if(!alert("Great win! It's a blowout victory for the Lakers!")) {
+                        window.location.reload();
+                    }; }, 500);
+                    break;
+
+                } else if (attemptsRemaining > 0 && lettersRemaining === 0) {
+                    answerBankDiv.innerHTML = answerBank.join(' ');
+                    setTimeout(function() { if(!alert("Lakers win! Great team victory!")) {
+                        window.location.reload();
+                    }; }, 500);
+                    break;
+
+                } else if ((attemptsRemaining === 0 && lettersRemaining > 0) || (lettersRemaining > attemptsRemaining)) {
+                    setTimeout(function() { if(!alert("You lose. Don't quit, try again!")) {
+                        window.location.reload();
+                    }; }, 500);
+                    break;
+                }
             }
         }
-    } 
-
-
-
-
-
-
-// Gamplay continuation requirements
-// while (lettersRemaining > 0 && attemptsRemaining > 0)
-
-//     console.log(answerBank.join(" "));
-
-
-
-//Guesses replace _ in answer bank
-if (userGuess.textContent > 1) {
-    alert("Please press only one character at a time");
-} else {
-    for (var j = 0; j < word.length; j++) {
-        if (word[j] === userGuess.textContent) {
-            answerBank[j] = userGuess.textContent;
-            lettersRemaining--;
-            console.log(answerBank);
-        }
     }
-} 
+}
 
-// Keeps the game rolling until all letters are guessed or until player runs out of allowed guesses
-//  while (lettersRemaining > 0 && attemptsRemaining > 0) {
-// // game happens
-// //reveal progress of player
-// //use event listeners/keyup to take player guesses
-// //update answer bank, remaining letters and attempts remaining
-//  }
 
-//  var fruits = ["Banana", "Pear", "Strawberry"];
-//  var energy = fruits.join(" ");
-//  console.log(energy);
-//  console.log(energy.split());
-//  console.log(energy.charAt(0));
 
-//  console.log(word.split('').join(','));
 
-//  console.log(word.split());
- 
+
 
 
 
